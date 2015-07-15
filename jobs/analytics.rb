df44fa09a203d1753745310e0201ef0bac47d374
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-07-15 23:56:29 +0300

    Extract GA query to GaQueryClient class

--- jobs/analytics.rb
+++ jobs/analytics.rb
@@ -5,89 +5,100 @@ require 'date'
 require 'yaml'
 
 API_VERSION = 'v3'
-
 CACHED_API_FILE = "#{ENV["TMPDIR"] || "/tmp/"}.ga-analytics-#{API_VERSION}.cache"
 
 ## Read app credentials from a file
 global_config = YAML.load_file('/etc/latcraft.yml')
-opts = global_config['ga'] || {}
+global_opts = global_config['ga'] || {}
 
-## Update these to match your own apps credentials in the ga_config.yml file
-service_account_email = opts['service_account_email']  # Email of service account
-key_file = opts['key_file']                            # File containing your private key
-key_secret = opts['key_secret']                        # Password to unlock private key
-ga_attributes_yml = opts['ga_attributes_yml']
-@profileID = opts['profile_id'].to_s                   # Analytics profile ID.
+class GaQueryClient
+  attr_reader :profileID
+  attr_reader :client
 
-@client = Google::APIClient.new(
-  :application_name => opts['application_name'],
-  :client_id => service_account_email,
-  :application_version => opts['application_version'])
+ def initialize(opts)
+    application_name = opts['application_name']
+    application_version = opts['application_version']
+    service_account_email = opts['service_account_email']  # Email of service account
+    key_file = opts['key_file']                            # File containing your private key
+    key_secret = opts['key_secret']                        # Password to unlock private key
+    @profileID = opts['profile_id'].to_s                   # Analytics profile ID.
 
-## Load our credentials for the service account
-key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
+    @client = Google::APIClient.new(
+      :application_name => application_name,
+      :client_id => service_account_email,
+      :application_version => application_version)
 
-@client.authorization = Signet::OAuth2::Client.new(
-  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-  :audience => 'https://accounts.google.com/o/oauth2/token',
-  :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-  :issuer => service_account_email,
-  :signing_key => key)
+    ## Load our credentials for the service account
+    key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
 
-@client.retries = 5
-## Request a token for our service account
-@client.authorization.fetch_access_token!
+    @client.authorization = Signet::OAuth2::Client.new(
+      :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+      :audience => 'https://accounts.google.com/o/oauth2/token',
+      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+      :issuer => service_account_email,
+      :signing_key => key)
 
-@analytics = nil
-## Load cached discovered API, if it exists. This prevents retrieving the
-## discovery document on every run, saving a round-trip to the discovery service.
-if File.exists? CACHED_API_FILE
-  File.open(CACHED_API_FILE) do |file|
-    @analytics = Marshal.load(file)
+    @client.retries = 5
+    ## Request a token for our service account
+    @client.authorization.fetch_access_token!
+
+    @analytics = nil
+    ## Load cached discovered API, if it exists. This prevents retrieving the
+    ## discovery document on every run, saving a round-trip to the discovery service.
+    if File.exists? CACHED_API_FILE
+      File.open(CACHED_API_FILE) do |file|
+        @analytics = Marshal.load(file)
+      end
+    else
+      @analytics = @client.discovered_api('analytics', API_VERSION)
+      File.open(CACHED_API_FILE, 'w') do |file|
+        Marshal.dump(@analytics, file)
+      end
+    end
   end
-else
-  @analytics = @client.discovered_api('analytics', API_VERSION)
-  File.open(CACHED_API_FILE, 'w') do |file|
-    Marshal.dump(@analytics, file)
+
+  ## Query Parameters Summary https://developers.google.com/analytics/devguides/reporting/core/v3/reference#q_summary
+  ## Funcation to query google for a set of analytics attributes
+ def query(start_date, end_date, dimension, metric, sort)
+    parameters = {
+      'ids' => "ga:" + @profileID,
+      'start-date' => start_date,
+      'end-date' => end_date,
+      'dimensions' => dimension,
+      'metrics' => metric,
+      'sort' => sort
+    }
+
+    result = @client.execute(:api_method => @analytics.data.ga.get, :parameters => parameters)
+    if result.status == 200 then
+      # Everything is OK
+    else
+      # Error, like HTTP 403, permission denied
+      # Rewrap in Google ClientError
+      raise Google::APIClient::ClientError.new "GA error #{result.data.error['code']}: #{result.data.error['message']}"
+    end
+
+    return result
   end
 end
 
+
+@client = GaQueryClient.new(global_opts)
 ## For the moment start and end dates defined here
-@startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
-@endDate = DateTime.now.strftime("%Y-%m-%d")
-
-## Query Parameters Summary https://developers.google.com/analytics/devguides/reporting/core/v3/reference#q_summary
-## Funcation to query google for a set of analytics attributes
-def query_ga (dimension, metric, sort)
-  parameters = {
-    'ids' => "ga:" + @profileID,
-    'start-date' => @startDate,
-    'end-date' => @endDate,
-    'dimensions' => dimension,
-    'metrics' => metric,
-    'sort' => sort
-  }
-
-  result = @client.execute(:api_method => @analytics.data.ga.get, :parameters => parameters)
-  if result.status == 200 then
-    # Everything is OK
-  else
-    raise Google::APIClient::ClientError.new "GA error #{result.data.error['code']}: #{result.data.error['message']}"
-  end
-
-  return result
-end
+start_date = DateTime.now.prev_month.strftime("%Y-%m-%d")
+end_date = DateTime.now.strftime("%Y-%m-%d")
 
 ## Dimensions and Metrics Reference: https://developers.google.com/analytics/devguides/reporting/core/dimsmets
 ## A single dimension data request to be retrieved from the API is limited to a maximum of 7 dimensions
 ## A single metrics data request to be retrieved from the API is limited to a maximum of 10 metrics
+ga_attributes_yml = global_opts['ga_attributes_yml']
 
 ## Set of dimensions and metrics to query in a file and iterate
 attributes = YAML.load_file(ga_attributes_yml)
 
 attributes.each_key { |key|
   outfile = File.new("#{key}.txt", "w")
-  gadata = query_ga(attributes[key]['dimension'], attributes[key]['metric'], attributes[key]['sort'])
+  gadata = @client.query(start_date, end_date, attributes[key]['dimension'], attributes[key]['metric'], attributes[key]['sort'])
   outfile.puts gadata.data.column_headers.map { |c| c.name.gsub("ga:","") }.join("\t")
   gadata.data.rows.each do |r|
     outfile.puts r.join("\t")
