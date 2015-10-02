d227aaf00d9720399470a96690c2402170b633e0
Andrey Adamovich <andrey@aestasit.com>
2015-10-02 19:52:44 +0300

    dynamic date calculation

--- jobs/schedule.rb
+++ jobs/schedule.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 require 'active_support/time'
 require 'uri'
 require 'yaml'
+require 'date'
 
 ###########################################################################
 # Load configuration parameters.
@@ -20,9 +21,8 @@ global_config = YAML.load_file('./config/latcraft.yml')
 SCHEDULER.every '1m', :first_in => 0 do |job|
   current_time = Time.now.in_time_zone('Europe/Riga')
   schedule = JSON.parse(open(global_config['schedule_data_file']) { |f| f.read })
-  # TODO: make generic query based on current date to show next event
-  next_event = schedule.select { |event| event['month'] == 'September 2015'}.first  
-  # TODO: filter subevents based on current time to not show things that already happened
+  current_month = "#{Date::MONTHNAMES[Date.today.month]} #{Date.today.year}"
+  next_event = schedule.select { |event| event['month'] == current_month}.first  
   sessions = next_event['schedule']
   sessions.each do |session|
     if !session['img'].nil? and !session['img'].empty? and !session['img'].start_with?('http')
