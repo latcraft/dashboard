677e0c7e5f1cc5d0258d0f1cdd4d19f41a7557e4
Andrey Adamovich <andrey@aestasit.com>
2015-11-21 12:58:01 +0200

    added basic room name calculation

--- jobs/devternity.rb
+++ jobs/devternity.rb
@@ -22,7 +22,7 @@ def to_min(time_code)
   return (time_code / 100) * 60 + time_code % 100
 end
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '1m', :first_in => 0 do |job|
   current_time = Time.now.in_time_zone('Europe/Riga')
   current_min  = current_time.hour * 60 + current_time.min
   schedule     = JSON.parse(open(global_config['devternity_data_file']) { |f| f.read }).first['schedule']
@@ -39,9 +39,9 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
   valid_slots = time_slots.select { |time_slot| to_min(time_slot[:time_code]) > current_min + 15 }  
   current_slots = valid_slots.select { |slot| slot[:time_code] == valid_slots.first[:time_code] }
   if current_slots && current_slots.size > 0
-    track1 = current_slots[0]
-    track2 = current_slots.size > 1 ? current_slots[1] : current_slots[0]
-    track3 = current_slots.size > 2 ? current_slots[2] : current_slots[0] 
+    track1 = (current_slots[0]).merge({ :room_name => 'T1' })
+    track2 = (current_slots.size > 1 ? current_slots[1] : current_slots[0]).merge({ :room_name => 'T2' })
+    track3 = (current_slots.size > 2 ? current_slots[2] : current_slots[0]).merge({ :room_name => 'T3' })
     send_event('track1', session: track1)
     send_event('track2', session: track2)
     send_event('track3', session: track3)
