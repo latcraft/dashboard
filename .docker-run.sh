7621759d37610560a97870e2f0f1e210774e299b
Leonids Maslovs <leonids.maslovs@knowledgeprice.com>
2016-06-08 23:49:11 +0300

    local env improvements

--- .docker-run.sh
+++ .docker-run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 
-docker run  --name dashboard      \
-  -h local-dashboard.latcraft.lv  \
-  -p 3030:3030                    \
-  -v "`pwd`:/vagrant"             \
-  -it                             \
-  "latcraft_dashboard:latest"
+docker run --name dashboard \
+ -h local-dashboard.latcraft.lv \
+ -p 3030:3030 \
+ -v "`pwd`:/vagrant" \
+ -v "`pwd`/.data/sqlite:/var/lib/sqlite" \
+ -it \
+ --rm \
+ "latcraft_dashboard:latest"
 
