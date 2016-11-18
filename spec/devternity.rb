248b5c716270881bae60772120d658dee12249d4
Andrey Adamovich <andrey@aestasit.com>
2016-11-18 15:52:47 +0200

    implemented some tests on top schedule logic

--- spec/devternity.rb
+++ spec/devternity.rb
@@ -1,21 +1,34 @@
 
 require_relative '../jobs/devternity.rb'
 
-describe "schedule" do
+def time(h, m) 
+  return Time.now.in_time_zone('Europe/Riga').change({hour: h, min: m})
+end
+
+tests = {
+  time(12, 55) => '13:40',
+  time(12, 56) => '13:40',
+  time(12, 57) => '13:40',
+  time(12, 58) => '13:40',
+  time(12, 59) => '13:40',
+}
+
+describe "schedule" do 
 
   before(:each) do
     @global_config = { 'devternity_data_file' => 'http://devternity.com/js/event.js' }
-    @events = {}
+    @now = Time.now
   end
 
-  it "should return" do
-    allow(Time).to receive(:now).and_return(2.hours.ago)
-    Devternity.send_schedule_updates @global_config, nil do |eventName, eventData| 
-      @events[eventName] = eventData
+  tests.each do |actualTime, expectedTime|
+    it "should return events starting on #{expectedTime} when the actual time is #{actualTime.strftime '%H:%M'}" do
+      allow(Time).to receive(:now).and_return(actualTime)
+      Devternity.send_schedule_updates @global_config, nil do |eventName, eventData| 
+        expect(eventData[:session][:time]).to eql(expectedTime) 
+      end
     end
-    puts "#{@events}"
-    # TODO: implement asserts
   end
+
 end
 
 
