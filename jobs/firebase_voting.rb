379d94f1a5b77e914cc947ed9cdbbb6189aca667
Andrey Adamovich <andrey@aestasit.com>
2018-11-18 16:43:26 +0200

    script for generating votes + additional filters

--- jobs/firebase_voting.rb
+++ jobs/firebase_voting.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require 'active_support/core_ext/enumerable'
+require 'active_support/time'
 require 'json'
 require 'yaml'
 require 'date'
@@ -15,16 +16,27 @@ $firebase_config = JSON.parse($firebase_json)
 $base_url = "https://#{$firebase_config['project_id']}.firebaseio.com/"
 $firebase_client = Firebase::Client.new($base_url, $firebase_json)
 
+def raw_votes(color = "green")
+  response = $firebase_client.get("votes", { :color => color })
+  raise "DT error #{response.code} (#{response.body})" unless response.success?
+  response.body
+end
+
+def today_votes(votes = raw_votes())
+  from = Time.now.in_time_zone('Europe/Riga').beginning_of_day
+  to   = Time.now.in_time_zone('Europe/Riga').end_of_day
+  votes.select { |id, vote| 
+    !vote["created"].nil? && 
+    vote["created"] >= from.to_i && 
+    vote["created"] <= to.to_i 
+  }
+end
+
 ###########################################################################
 # Job's schedules.
 ###########################################################################
 
 SCHEDULER.every '1m', :first_in => 0 do |job| 
-  response = $firebase_client.get("votes", { :color => "green" })
-  # response = $firebase_client.push("votes", {
-  #   :device => 'test_1',
-  #   :color => 'green',
-  #   :created => Firebase::ServerValue::TIMESTAMP
-  # })
-  send_event('votes', { current: response.body.length })
+  votes = today_votes()
+  send_event('votes', { current: votes.length })
 end
